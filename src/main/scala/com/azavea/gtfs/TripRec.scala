fba9b938b2d6a7e122e1ba1fb0fcba07e5e3e83e
Eugene Cheipesh <echeipesh@gmail.com>
2014-03-17 18:39:25 -0400

    TripRecs are becoming self-aware
    They can recognize same and segregate

--- src/main/scala/com/azavea/gtfs/TripRec.scala
+++ src/main/scala/com/azavea/gtfs/TripRec.scala
@@ -2,6 +2,7 @@ package com.azavea.gtfs
 
 import com.github.nscala_time.time.Imports._
 import scala.Some
+import scala.collection.mutable
 
 case class TripRec(
   trip_id: TripId,
@@ -27,5 +28,56 @@ case class TripRec(
           stops = stopTimes.view.map{ _.toStopTime(date)}
         ))
     }
+  }
+
+  /**
+   * In order to be sameish two trips need to:
+   * - belong to same service
+   * - belong to same route
+   * - have the same stops
+   * - same travel time between stops
+   * - same time at each stop
+
+   * In general the two trips should be re-creatable from one trip and frequency record.
+   */
+  def sameish(that: TripRec): Boolean = {
+    //This is a super-safe assumption, it is in fact possible to have two services that run on same days
+    if (this.service_id != that.service_id) return false
+    if (this.route_id != that.route_id) return false
+    if (that.stopTimes.length != this.stopTimes.length) return false
+    val l_start = this.stopTimes.head.arrival_time
+    val r_start = that.stopTimes.head.arrival_time
+
+    for ( (l,r) <- stopTimes zip that.stopTimes) {
+      if (l.stop_id != r.stop_id) return false
+      if (l.arrival_time - l_start != r.arrival_time - r_start) return false
+      if (l.departure_time - l_start != r.departure_time - r_start) return false
+    }
+    true
+  }
+}
+
+object TripRec {
+  def bin(trips: Seq[TripRec]) = {
+    val bins = mutable.Set(mutable.Set(trips.head))
+
+    //go where you belong
+    def belongs(t: TripRec): Boolean = {
+      for (b <- bins) {
+        if (t sameish b.head) {
+          b += t
+          return true
+        }
+      }
+      false
+    }
+
+    for (t <- trips.tail) {
+      if (! belongs(t)) {
+        bins += mutable.Set(t) //You're in a class of you own!
+      }
+    }
+
+    bins
   }
 }
--- src/test/scala/com/azavea/gtfs/TripSpec.scala
+++ src/test/scala/com/azavea/gtfs/TripSpec.scala
@@ -16,12 +16,46 @@ class TripSpec extends FlatSpec with Matchers {
     }
   }
 
+  val trip1 = TripRec("T1","SR1","R1","Go Home",
+    List(
+      StopTimeRec("S1","T1", 1, 0.seconds, 1.minute),
+      StopTimeRec("S2","T1", 1, 10.minutes, 11.minutes),
+      StopTimeRec("S3","T1", 1, 15.minutes, 16.minutes)
+    )
+  )
+  val trip2 = TripRec("T2","SR1","R1","Go Home Again",
+    List(
+      StopTimeRec("S1","T2", 1, 1.minute + 0.seconds, 1.minute + 1.minute),
+      StopTimeRec("S2","T2", 1, 1.minute + 10.minutes, 1.minute + 11.minutes),
+      StopTimeRec("S3","T2", 1, 1.minute + 15.minutes, 1.minute + 16.minutes)
+    )
+  )
+  val trip3 = TripRec("T2","SR1","R1","Go Home Again",
+    List(
+      StopTimeRec("S1","T2", 1, 1.minute + 0.seconds, 1.minute + 1.minute),
+      StopTimeRec("S2","T2", 1, 1.minute + 10.minutes, 1.hour + 11.minutes), //long break here
+      StopTimeRec("S3","T2", 1, 1.hour + 15.minutes, 1.hour + 16.minutes)
+    )
+  )
+
   "TripRec" should "generate timed Trips" in {
     val trip = data.tripById("T1")
-
     val t = trip(new LocalDate(2013, 2, 2))
-
     t(0).stops(0).arrival.toLocalTime should equal (new LocalTime(10,00,00))
-    //t.foreach(printTrip)
+  }
+
+  it should "recognize a shameish trip" in {
+    assert(trip1 sameish trip2, "Two trips that differ only in time offset are sameish")
+    assert(! (trip1 sameish trip3), "Two trips that differ in stop intervals are not sameish")
+  }
+
+  it should "know how to segregate" in {
+    val bins = TripRec.bin(trip1 :: trip2 :: trip3 :: Nil)
+    val big = bins.find(_.size == 2).get
+    val small = bins.find(_.size == 1).get
+
+    big should contain (trip1)
+    big should contain (trip2)
+    small should contain (trip3)
   }
 }
